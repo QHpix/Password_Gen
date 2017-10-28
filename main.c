209f7da18ae5a0fcf005d41efbc522fc61b8e3d7
QHpix <qvh2002@gmail.com>
2017-10-28 12:11:48 +0200

    Alpha release

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+char chrs[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!_";
 
 int main()
 {
-
+	int pl, len = strlen(chrs), r, i;
+	srand(time(NULL));
+	char *passw;
+	printf("Enter the length of the password: ");
+	scanf("%d", &pl);
+	passw = (char *)malloc(pl*sizeof(char));
+	for(i = 0; i < pl;i++)
+	{
+		r = rand() % len;
+		passw[i] = chrs[r];
+	}
+	printf("%s\n", passw);
 	return 0;
 }
